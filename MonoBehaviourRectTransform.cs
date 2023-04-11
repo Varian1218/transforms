cbbd576f717dad9835dc4cc78781d02ace5ee225
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-11 22:20:45 +0700

    1.9.0

---
+++ MonoBehaviourRectTransform.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Vector2 = System.Numerics.Vector2;
+
+namespace Transforms
+{
+    public class MonoBehaviourRectTransform : MonoBehaviour, IRectTransform
+    {
+        private IRectTransform _impl;
+
+        private IRectTransform Impl => _impl ??= new UnityRectTransform
+        {
+            Impl = GetComponent<RectTransform>()
+        };
+
+        public float Height => Impl.Height;
+
+        public Vector2 Size => Impl.Size;
+
+        public ITransform Transform => Impl.Transform;
+
+        public float Width => Impl.Width;
+    }
+}
